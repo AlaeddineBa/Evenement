9d7fb30eb83a7884c7d7509f5e0752ed8038624a
AlaeddineBa <alaeddine.baghdadi@outlook.com>
2016-01-28 19:16:26 +0100

    Bouton add update delete edit

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,43 +1,78 @@
-function AppCtrl($scope, $http){
-	console.log("Hello Word frm Conroller");
 
-var refresh = function(){
-	$http.get('/evenementlist').success(function(response){
-		console.log("I got the date I requested");
-		console.log("ID"+ response.id);
-		$scope.evenementlist = response;
-		$scope.evenement = "";
-	});
-};
+var app = angular.module('app',['naif.base64']); 
+
+app.controller('AppCtrl',function($scope, $http){
+
+	$scope.logoPick = 'css/img/pic.png';
+	$scope.evenementlist = [];
+	$scope.event = {};
+	$scope.pick = '';
+
+	$scope.query = '';
+
+	$scope.opened = false;
+
+	var refresh = function(){
+		$http.get('/getEventList')
+		.then(function successCallback(response) {
+	    	$scope.evenementlist = response.data;
+	    	$scope.event = "";
+	    	$scope.logoPick = 'css/img/pic.png';
+	    },function error(err){
+	    	console.log(err);
+		  });
+	  ;
+	};
+
 	refresh();
-	$scope.addEvenement = function() {
-		console.log($scope.evenement);
-		$http.post('/evenementlist', $scope.evenement).success(function(response){
-			console.log(response);
+
+	$scope.onLoad = function(e, reader, file, fileList, fileOjects, fileObj){
+		$scope.event.base64 = fileObj.base64;
+		$scope.logoPick = 'data:image/png;base64,'+fileObj.base64;
+		$scope.pick = fileObj.base64;
+	}
+
+	
+	$scope.addEvent = function(event) {  
+		if(Object.keys($scope.event).length < 3){
+			alert('SVP remplisez tous les champs');
+			return false;
+		}
+		if($scope.event.base64 == undefined || $scope.event.base64 == null ||
+				$scope.event.base64 == '' ){
+			alert('SVP Choisisez un logo');
+			return false;
+		}
+	var title = event.title;
+  	var desc = event.desc;
+  	var date = event.date;
+  	var type = event.type;
+  	$scope.evenementlist.push({'title':title,'desc':desc,'date':date, 'type':type, 'base64':$scope.pick});
+		$http.post('/addEventList',event)
+		.then(function successCallback(response) {
 			refresh();
-		});
+	    },function error(err){
+	    	console.log(err);
+		  });
+	  ;
 	};
 
-	$scope.removeEvenement = function(id) {
-		console.log(id);
-		$http.delete('/evenementlist/' +id).success(function(response){
+	$scope.removeEvent = function(id,index) {
+		$http.delete('/delEventList/' +id).success(function(response){
 			refresh();
 		});
 	};
 	
-	$scope.editEvenement = function(id) {
-		console.log(id);
-		$http.get('/evenementlist/' +id).success(function(response){
-		$scope.evenement = response;
-		});
+	$scope.editEvent = function(event) {
+		$scope.logoPick = 'data:image/png;base64,'+event.base64;
+		$scope.event = event;
 	};
 
-	$scope.update = function() {
-		console.log($scope.evenement._id);
-		$http.put('/evenementlist/' +$scope.evenement._id, $scope.evenement).success(function(response){
+	$scope.updateEvent = function(event) {
+		$http.put('/updateEvent/' +event._id,event).success(function(response){
 			refresh();
 		});
 	};
 
 	
-}
+});
--- server.js
+++ server.js
@@ -1,47 +1,37 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('evenementlist',['evenementlist']);
+var db = mongojs('evenementlist',['eventlist']);
 var bodyParser = require('body-parser');
 
 
 	app.use(express.static(__dirname + "/public"));
 	app.use(bodyParser.json());
 
-	app.get('/evenementlist', function (req, res){
-		console.log("I receive a GET Request");
-		db.evenementlist.find(function (err, docs){
+	app.get('/getEventList', function (req, res){
+		db.eventlist.find(function (err, docs){
 			res.json(docs);
 		});
 	});
-	app.post('/evenementlist', function (req, res){
-		console.log(req.body);
-		db.evenementlist.insert(req.body, function (err, doc){
+
+	app.post('/addEventList', function (req, res){
+		db.eventlist.insert(req.body, function (err, doc){
+			res.json(doc);
+		});
+	});
+
+	app.delete('/delEventList/:id', function (req, res){
+		var id = req.params.id;
+		db.eventlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
 			res.json(doc);
 		})
 	});
 
-	app.delete('/evenementlist/:id', function (req, res){
+	app.put('/updateEvent/:id', function (req, res){
 		var id = req.params.id;
 		console.log(id);
-		db.evenementlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
-			res.json(doc);
-		})
-	});
-
-	app.get('/evenementlist/:id', function (req, res){
-		var id = req.params.id;
-		console.log("I receive a GET Request");
-		db.evenementlist.findOne({_id: mongojs.ObjectId(id)}, function (err, docs){
-			res.json(docs);
-		})
-	});
-
-	app.put('/evenementlist/:id', function (req, res){
-		var id = req.params.id;
-		console.log(req.body);
-		db.evenementlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
-		update: {$set: {titre: req.body.titre, description: req.body.description, date: req.body.date}},
+		db.eventlist.findAndModify({query: {_id: mongojs.ObjectId(req.body._id)},
+		update: {$set: {title: req.body.title, desc: req.body.desc, date: req.body.date, type: req.body.type, base64:req.body.base64}},
 		new: true}, function (err, docs){
 			res.json(docs);
 		});
